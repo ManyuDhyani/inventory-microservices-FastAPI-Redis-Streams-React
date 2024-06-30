5d62a5444e2b28ed362d84a8edcca855d610aa8f
ManyuDhyani <manyudhyani@gmail.com>
2024-06-30 18:24:28 -0400

    Inventory CRUD

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from redis_om import get_redis_connection, HashModel
+import json
 
 app = FastAPI()
 app.add_middleware(
@@ -9,13 +10,22 @@ app.add_middleware(
     allow_methods=['*'],
     allow_headers=['*']
 )
+
+# Load configuration from a JSON file
+with open('config.json', 'r') as f:
+    config = json.load(f)
+
+
 # Set up the Redis connection
 redis = get_redis_connection(
-    host="localhost",  # Change this to your Redis host if different
-    port=6379,         # Default Redis port
-    password=None,     # Set your Redis password if any
+    host=config['redis']['host'],
+    port=config['redis']['port'],
+    password=config['redis']['password'],
     decode_responses=True
 )
+
+info = redis.info()
+print(info['redis_version'])
 # Define a route to test the Redis connection
 @app.get("/redis")
 async def test_redis():
@@ -32,13 +42,28 @@ class Product(HashModel):
         database = redis
 
 @app.get("/products")
-async def all():
-    try:
-        product_keys = Product.all_pks()
-        return product_keys
-    except Exception as e:
-        return {"error": str(e)}
+def all():
+    return [format(pk) for pk in Product.all_pks()]
 
-if __name__ == "__main__":
-    import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+def format(pk: str):
+    product = Product.get(pk)
+
+    return {
+        'id': product.pk,
+        'name': product.name,
+        'price': product.price,
+        'quantity': product.quantity
+    }
+
+@app.post('/products')
+def create(product: Product):
+    return product.save()
+
+
+@app.get('/products/{pk}')
+def get(pk: str):
+    return Product.get(pk)
+
+@app.delete('/products/{pk}')
+def delete(pk: str):
+    return Product.delete(pk)
