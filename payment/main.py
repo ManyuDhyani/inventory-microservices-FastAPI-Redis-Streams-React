694bb6f89884cd42b8d4e62935feedb461ab6eb0
ManyuDhyani <manyudhyani@gmail.com>
2024-07-01 23:36:43 -0400

    inventory consumer with Redis Stream

--- payment/main.py
+++ payment/main.py
@@ -1,9 +1,11 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
+from fastapi.background import BackgroundTasks
 from redis_om import get_redis_connection, HashModel
 from starlette.requests import Request
 import json
 import requests
+import time
 
 app = FastAPI()
 app.add_middleware(
@@ -37,8 +39,12 @@ class Order(HashModel):
     class Meta:
         database = redis
 
+@app.get('/orders/{pk}')
+def get(pk: str):
+    return Order.get(pk)
+
 @app.post('/orders')
-async def create(request: Request):  # id, quantity
+async def create(request: Request, background_tasks: BackgroundTasks):  # id, quantity
     body = await request.json()
 
     req = requests.get('http://localhost:8000/products/%s' % body['id'])
@@ -53,10 +59,13 @@ async def create(request: Request):  # id, quantity
         status='pending'
     )
     order.save()
-    order_completed(order)
+    background_tasks.add_task(order_completed, order)
 
     return order
 
 def order_completed(order: Order):
+    time.sleep(5)
     order.status = 'complete'
     order.save()
+
+    redis.xadd('order_completed', order.dict(), '*')
